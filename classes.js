eb30f76500e00be8b13b01f72278d02f1340bf29
nicole-allen679 <nicole.allen679@gmail.com>
2021-04-15 14:54:05 -0400

    classes/child practice

--- classes.js
+++ classes.js
@@ -3,9 +3,32 @@ class Product {
     this.name = productName
   }
   display() {
-    console.log('For Sale: ' + this.name)
+    return 'For Sale: ' + this.name
   }
 }
 
-let prod1 = new Product('shirt')
-prod1.display()
+class item extends Product {
+  constructor(productName, sku, style, qty) {
+    super(productName)
+    this.sku = sku
+    this.style = style
+    this.qty = qty
+  }
+  itemDisplay() {
+    return this.display() + `, ${this.style} (${this.sku}) (${this.qty})`
+  }
+  sellSome(qtySold) {
+    this.qty -= qtySold
+  }
+  getSome(qtyReceived){
+      this.qty += qtyReceived
+  }
+}
+
+let item1 = new item('shirt', 'TKL', 'large black', 10)
+console.log(item1.display())
+console.log(item1.itemDisplay())
+item1.sellSome(3)
+console.log(item1.itemDisplay())
+item1.getSome(25)
+console.log(item1.itemDisplay())
